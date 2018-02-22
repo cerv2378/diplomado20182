3273b1dfe6907ec45fdb6cfd919f011441dde39d
Christian Erick <cerv2378.2378@gmail.com>
2018-02-21 22:04:50 -0600

    empezando mi app

---
+++ Tareas/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by d182_Christian_R on 16/02/18.
+//  Copyright © 2018 Christian Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

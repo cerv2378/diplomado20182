45aa4ce88df5457fe0299b40469299cd6ef8d2c9
Christian Erick <cerv2378.2378@gmail.com>
2018-02-21 23:39:03 -0600

    aun trabajando en la app

--- Tareas/Quiz/Quiz/ViewController.swift
+++ Tareas/Quiz/Quiz/ViewController.swift
@@ -10,18 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    
+    
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes" ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnwer(_ sender: UIButton) {let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    @IBOutlet weak var questionLabel: UILabel!
+            super.viewDidLoad()
+            questionLabel.text = questions[currentQuestionIndex]
+        }
     
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 

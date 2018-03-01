b25a8dbff98e3c1dc3b38a49cd695171768af207
Christian Erick <cerv2378.2378@gmail.com>
2018-03-01 01:46:19 -0600

    ya modifique una parte de el programa de WhatHappenedToMe

--- Tareas/whatHappenedToMe/prueba/ViewController.swift
+++ Tareas/whatHappenedToMe/prueba/ViewController.swift
@@ -10,18 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-     weak var outtletText: UITextField!
-    @IBOutlet weak var aoutletButton: UIButton!
+    
+    // se elimino los label y el boton que se agregaron de mas al mainstoryboar como tambien el label como  lo volvi a unir con view el botton se agregue accion
+    
+    @IBAction func aoutletButton(_ sender: UIButton) {
+        
+        outtletText.text = "You are a genius "
+    }
+    
+    @IBOutlet var outtletText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        aoutletButton.setTitle("Touch me!", for: .normal)
+        //aoutletButton.setTitle("Touch me!", for: .normal)
         
     }
 
-    func actionButton(_ sender: Any) {
-        outtletText.text = "You are a genius "
-    }
+    // func actionButton(_ sender: Any) {
+    //outtletText.text = "You are a genius "
+    //}
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

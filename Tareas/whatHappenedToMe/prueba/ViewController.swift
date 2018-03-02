32fdbf1b34957a9f02212a3a587fe812121855bc
Christian Erick <cerv2378.2378@gmail.com>
2018-03-02 02:10:11 -0600

    ya quedo la aplicacion de whatHappenedToMe

--- Tareas/whatHappenedToMe/prueba/ViewController.swift
+++ Tareas/whatHappenedToMe/prueba/ViewController.swift
@@ -11,24 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    // se elimino los label y el boton que se agregaron de mas al mainstoryboar como tambien el label como  lo volvi a unir con view el botton se agregue accion
+    // se elimino un texfield y el boton que se agregaron de mas, luego deje comentado la funcion de agregar texto a el textfield y el de cambiarle el nombre ya que no me queria correr bien y de ahi uni de nuevo el boton con el viewcontroller y le di accion al boton ya que si no le daba la acccion y usaba la funcion de accion de el boton no queria trabajar por eso le meti al boton accion y ya pudo trabajar mejor el programa.
     
-    @IBAction func aoutletButton(_ sender: UIButton) {
-        
-        outtletText.text = "You are a genius "
-    }
+    
+   
+    
     
     @IBOutlet var outtletText: UITextField!
     
-    
+    @IBAction func aoutletButton(_ sender: Any) {
+        outtletText.text = "You are a genius "
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //aoutletButton.setTitle("Touch me!", for: .normal)
         
     }
-
-    // func actionButton(_ sender: Any) {
+    //func actionButton(_ sender: Any) {
     //outtletText.text = "You are a genius "
     //}
 
